e21cc7bb1eeef7115a12e1c583ff9d2db9d93756
Cristobaljaram7 <cristobaljaramora@gmail.com>
2025-02-07 17:31:41 -0300

    update13

--- src/Datos.jsx
+++ src/Datos.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import './datos.css'
 
 const Datos = () => {
   return (
-    <div>
-      <span>datos</span>
-    </div>
+    <section className="backLine">
+        <h1 className="title">PROFILE</h1>
+        
+        <h2>los datos esatan en la base de datos par aque no se boren pq son datos datosos</h2>
+    </section>
   )
 }
 
